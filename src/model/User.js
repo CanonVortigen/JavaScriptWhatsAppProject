9fa774a726099c2cf6066f49b0bc646ce9717d76
CanonVortigen <canonpendragon@outlook.com>
2024-04-26 09:09:51 -0300

    Updating profile data and adding contacts

--- src/model/User.js
+++ src/model/User.js
@@ -1,7 +1,44 @@
 import { Firebase } from '../util/Firebase.js';
-import { ClassEvent } from '../util/ClassEvent.js';
+import { Model } from './Model.js';
 
-export class User extends ClassEvent {
+export class User extends Model {
+
+    constructor(id) {
+
+        super();       
+        if (id) this.getById(id);
+
+    }
+
+    get name() { return this._data.name; }
+    set name(value) { this._data.name = value; }
+
+    get email() { return this._data.email; }
+    set email(value) { this._data.email = value; }
+
+    get photo() { return this._data.photo; }
+    set photo(value) { this._data.photo = value; }
+
+    getById(id) {
+
+        return new Promise((s, f)=>{
+
+            User.findByEmail(id).onSnapshot(doc => {
+
+                this.fromJSON(doc.data());
+                s(doc);
+
+            });
+
+        });
+
+    }
+
+    save() {
+
+        return User.findByEmail(this.email).set(this.toJSON());
+
+    }
     
     static getRef() {
 
@@ -12,6 +49,12 @@ export class User extends ClassEvent {
     static findByEmail(email) {
 
         return User.getRef().doc(email);
+
+    }
+
+    addContact(contact) {
+
+        return User.getRef().doc(this.email).collection('contacts').doc(btoa(contact.email)).set(contact.toJSON());
 
     }
 
